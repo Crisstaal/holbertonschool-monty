f8840ef206b5f66d2b2d572166d04e28acb47eb6
“github <you@example.com>
2023-12-21 14:05:28 -0800

    please monty work

--- monty.h
+++ monty.h
@@ -43,8 +43,6 @@ void free_stacks(stack_t **stacks);
 int init_stack(stack_t **stacks);
 int check_mode(stack_t *stacks);
 void free_token(void);
-unsigned int arr_len(void);
-int run_monty(FILE *script_fd);
 void set_error(int errors);
 void parse_stack_operation(stack_t **stack, unsigned int line_number);
 void (*get_operation(char *op))(stack_t**, unsigned int);
