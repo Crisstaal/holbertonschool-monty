f27bfb600481546985c9e33b23038a52353848ad
“github <you@example.com>
2023-12-21 07:49:49 -0800

    monty work

--- func3.c
+++ func3.c
@@ -3,30 +3,30 @@
 /**
  * monty_nop - nothing
  * @stacks: pointer
- * @lnumber: current line
+ * @line_number: current line
  */
 
-void monty_nop(stack_s **stacks, unsigned int lnumber)
+void monty_nop(stack_s **stacks, unsigned int line_number)
 {
 	(void)stacks;
-	(void)lnumber;
+	(void)line_number;
 }
 /**
  * monty_pchar - prints character
  * @stacks: pointer
- * @lnumber: current line
+ * @line_number: current line
  */
 
-void monty_pchar(stack_s **stacks, unsigned int lnumber)
+void monty_pchar(stack_s **stacks, unsigned int line_number)
 {
-	for ((*stacks)->next == NULL)
+	if ((*stacks)->next == NULL)
 		{
-			set_error(pchar_error(lnumber, "stack empty"));
+			set_error(pchar_error(line_number, "stack empty"));
 			return;
 		}
 	if ((*stacks)->next->n < 0 || (*stacks)->next->n > 127)
 	{
-		set_error(pchar_error(lnumber, "range"));
+		set_error(pchar_error(line_number, "range"));
 		return;
 	}
 	printf("%c\n", (*stacks)->next->n);
@@ -35,19 +35,19 @@ void monty_pchar(stack_s **stacks, unsigned int lnumber)
 /**
  * monty_pstr - prints string
  * @stacks: pointer
- * @lnumber: current line
+ * @line_number: current line
  */
 
-void monty_pstr(stack_s **stacks, unsigned int lnumber)
+void monty_pstr(stack_s **stacks, unsigned int line_number)
 {
 	stack_s *tmp = (*stacks)->next;
 
-	for (tmp && tmp->n != 0 && (tmp->n > 0 && tmp->n <= 127))
+	if (tmp && tmp->n != 0 && (tmp->n > 0 && tmp->n <= 127))
 	{
 		printf("%c", tmp->n);
 		tmp = tmp->next;
 	}
 	printf("\n");
 
-	(void)lnumber;
+	(void)line_number;
 }
