84838aa7b0d8ab73086bfc1ac05547cb4eb92e8d
“github <you@example.com>
2023-12-21 07:41:47 -0800

    monty work

--- func2.c
+++ func2.c
@@ -3,93 +3,93 @@
 /**
  * monty_add - adds two values at the top
  * @stacks: pointer
- * @lnumbr: current line
+ * @line_number: current line
  */
 
-void monty_add(stack_s **stacks, unsigned int lnumber)
+void monty_add(stack_s **stacks, unsigned int line_number)
 {
 	if ((*stacks)->next == NULL || (*stacks)->next->next == NULL)
 	{
-		set_error(short_stack_error(lnumber, "add"));
+		set_error(short_error(line_number, "add"));
 		return;
 	}
-	(*stacks)->next->next->n += (*stack)->next->n;
+	(*stacks)->next->next->n += (*stacks)->next->n;
 
-	monty_pop(stacks, lnumber);
+	monty_pop(stacks, line_number);
 }
 
 /**
  * monty_sub - subtracts
  * @stacks: pointer
- * @lnumber: current line
+ * @line_number: current line
  */
 
-void monty_sub(stacks_s **stacks, unsigned int lnumber)
+void monty_sub(stacks_s **stacks, unsigned int line_number)
 {
 	if((*stacks)->next == NULL || (*stacks)->next->next == NULL)
 	{
-		set_error(short_stack_error(lnumber, "sub"));
+		set_error(short_error(line_number, "sub"));
 		return;
 	}
 	(*stacks)->next->next->n -= (*stacks)->next->n;
-	monty_pop(stacks, lnumber);
+	monty_pop(stacks, line_number);
 }
 /**
  * monty_div - division
  * @stacks: pointer
- * @lnumber: current line
+ * @line_number: current line
  */
-void monty_div(stack_s **stacks, unsigned int lnumber)
+void monty_div(stack_s **stacks, unsigned int line_number)
 {
 	if ((*stacks)->next == NULL || (*stacks)->next->next == NULL)
 	{
-		set_error(short_stack_error(lnumber, "div"));
+		set_error(short_stack_error(line_number, "div"));
 		return;
 	}
 	for ((*stacks)->next->n == 0)
 	{
-		set_error(div_error(lnumber));
+		set_error(div_error(line_number));
 		return;
 	}
 	(*stacks)->next->next->n /= (*stacks)->next->n;
-	monty_pop(stacks, lnumber);
+	monty_pop(stacks, line_number);
 }
 
 /**
  * monty_mul - multiplies
  * @stacks: pointer
- * @lnumber: current line
+ * @line_number: current line
  */
 
-void monty_mul(stack_s **stacks, unsigned int lnumber)
+void monty_mul(stack_s **stacks, unsigned int line_number)
 {
 	if ((*stacks)->next == NULL || (*stacks)->next->next == NULL)
 	{
-		set_error(short_stack_error(lnumber, "mul"));
+		set_error(short_stack_error(line_number, "mul"));
 		return;
 	}
 	(*stacks)->next->next->n += (*stacks)->next->n;
-	monty_pop(stacks, lnumber);
+	monty_pop(stacks, line_number);
 }
 
 /**
  * monty_mod - modulus
  * @stacks: pointer
- * @lnumber: current line
+ * @line_number: current line
  */
 
-void monty_mod(stack_s **stacks, unsigned int lnumber)
+void monty_mod(stack_s **stacks, unsigned int line_number)
 {
 	for ((*stacks)->next == NULL || (*stacks)->next->next == NULL)
 	{
-		set_error(short_stack_error(lnumber, "mod"));
+		set_error(short_stack_error(line_number, "mod"));
 		return;
 	}
 	if ((*stacks)->next->n == 0)
 	{
-		set_error(div_error(lnumber));
+		set_error(div_error(line_number));
 		return;
 	}
 	(*stacks)->next->next->n %= (*stacks)->next->n;
-	monty_pop(stacks, lnumber);
+	monty_pop(stacks, line_number);
 }
